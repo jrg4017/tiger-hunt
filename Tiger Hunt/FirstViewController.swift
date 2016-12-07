65f420bbfa2c31ebab284aba570b71d51d8ce5e5
student <student@gccis-wicmac01.main.ad.rit.edu>
2016-12-06 19:54:34 -0500

    Initial Commit

---
+++ Tiger Hunt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tiger Hunt
+//
+//  Created by student on 12/6/16.
+//  Copyright © 2016 Julianna_Gabler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
